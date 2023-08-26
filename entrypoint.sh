e68e2d5525babc5cd011064c0f0f04e2699dd037
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-08-27 02:05:52 +0530

    asd

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR Number - $pull_request_number
 
 # Use the Random API to fetch a random funny GIF
-giphy_response=$(curl "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank you&rating=g")
+giphy_response=$(curl "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
 echo Giphy Response - $giphy_response
 
 # Extract the GIF URL from the Giphy response
