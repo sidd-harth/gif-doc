38073e4741b49fd06e0f3187f4f56722ce2d7001
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-08-27 01:59:06 +0530

    asd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,19 @@
 GITHUB_TOKEN=$1
 GIPHY_API_KEY=$2
 
+echo GHEvent - $GITHUB_EVENT_PATH
+
 # Get the pull request number from the GitHub event payload
 pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-echo $pull_request_number
+echo PR Number - $pull_request_number
 
 # Use the Random API to fetch a random funny GIF
-giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank you&rating=g")
-echo $giphy_response
+giphy_response=$(curl "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank you&rating=g")
+echo Giphy Response - $giphy_response
 
 # Extract the GIF URL from the Giphy response
 gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
-echo $gif_url
+echo URL - $gif_url
 
 # # Use the Random API to fetch a random funny GIF
 # giphy_response=$(curl -s "https://api.humorapi.com/gif/search?api-key=8fbb1119c04e46c1bcfbf3fae20935e6&query=random")
