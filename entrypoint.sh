df53aba38084c14e3a5b89d27baa5b5109d4d0f1
Barahalikar Siddharth <barahalikar.siddharth@gmail.com>
2023-10-05 00:19:52 +0530

    updated sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,34 +4,27 @@
 GITHUB_TOKEN=$1
 GIPHY_API_KEY=$2
 
-echo GHEvent - $GITHUB_EVENT_PATH
+GHEvent - `cat $GITHUB_EVENT_PATH`
+echo $GHEvent
 
 # Get the pull request number from the GitHub event payload
 pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR Number - $pull_request_number
 
 # Use the Random API to fetch a random funny GIF
-giphy_response=$(curl "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
+giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
 echo Giphy Response - $giphy_response
 
 # Extract the GIF URL from the Giphy response
 gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
-echo URL - $gif_url
-
-# # Use the Random API to fetch a random funny GIF
-# giphy_response=$(curl -s "https://api.humorapi.com/gif/search?api-key=8fbb1119c04e46c1bcfbf3fae20935e6&query=random")
-# echo $giphy_response
-
-# # Extract the Random URL from the Giphy response
-# gif_url=$(echo "$giphy_response" | jq --raw-output .images[0].url)
-# echo $gif_url
+echo GIPHY_URL - $gif_url
 
 # Create a comment with the GIF on the pull request
-comment_response=$(curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
+comment_response=$(curl -sX POST -H "Authorization: token $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   -d "{\"body\": \"### PR - #$pull_request_number has been assigned. \n ![GIF]($gif_url) \n ### 🎉 Thank you for this contribution!\"}" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_request_number/comments")
 
 # Extract and print the comment URL from the comment response
-comment_url=$(echo "$gif_url")
+comment_url=$(echo "$comment_response.url")
 echo "Comment URL: $comment_url"
